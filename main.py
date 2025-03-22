c09c43ff4dd9f9a907e7d3010f787003c383cd4d
MatheusCanteri <canterimatheu@gmail.com>
2025-03-22 08:41:28 -0300

    Criação do BD local e alterações no arquivo main

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from indb import generate_products
+
 app = FastAPI()
 
+products = generate_products()
+
 @app.get('/')
-def get_languages():
-    return {"success" : "funcionou"}
+def get_products():
+    return {"products" : products}
