38f5403076c18561225a8256024d087d695517ac
MatheusCanteri <canterimatheu@gmail.com>
2025-04-27 13:29:17 -0300

    Retirando o asyncio

--- tests/test_product_api.py
+++ tests/test_product_api.py
@@ -3,7 +3,6 @@ from product import Product
 
 BASE_URL = 'http://localhost:8000' 
 
-@pytest.mark.asyncio
 def test_should_get_list_of_products():
     response = requests.get(f'{BASE_URL}/products')
     assert response.status_code == 200
@@ -12,7 +11,6 @@ def test_should_get_list_of_products():
     assert isinstance(data["products"], list)
     assert len(data["products"]) >= 1 
 
-@pytest.mark.asyncio
 def test_should_insert_new_product():
     product = {"name": "Mouse Gamer", "price": 250.0}
     response = requests.post(f'{BASE_URL}/products', json=product)
@@ -20,20 +18,17 @@ def test_should_insert_new_product():
     data = response.json()
     assert data["status"] == "inserted"
 
-@pytest.mark.asyncio
 def test_should_have_new_product_after_insert():
     response = requests.get(f'{BASE_URL}/products')
     products = response.json()["products"]
     names = [product["name"] for product in products]
     assert "Mouse Gamer" in names
 
-@pytest.mark.asyncio
 def test_should_not_accept_invalid_product():
     invalid_product = {"name": "Teclado Mecânico", "price": "caro"}
     response = requests.post(f'{BASE_URL}/products', json=invalid_product)
     assert response.status_code == 422  
 
-@pytest.mark.asyncio
 def test_product_price_should_be_positive():
     response = requests.get(f'{BASE_URL}/products')
     products = response.json()["products"]
